e3f7e3ecca207608ae9ff654efada7b958394b47
mukul1107 <mukulsingh3078@gmail.com>
2024-04-14 22:44:11 +0530

    Able to get the data of my followers only through an array passed.

--- script.js
+++ script.js
@@ -1,33 +1,32 @@
+
 const img = document.querySelector("img")
-const para = document.querySelector("#followers")
-const userName = document.querySelector("#user-name")
+const para = document.querySelector(".followers")
+const userName = document.querySelector(".user-name")
 
 function getName(arr) {
     let randName = Math.floor(Math.random() * arr.length);
     return arr[randName];
 }
-
+const nameArr = ["Anil-choudhary-505", "CoDesign-Spa27", "Mohitsingh1999", "mukul1107"]
+const name = getName(nameArr)
+console.log(name)
 const xmlreq = new XMLHttpRequest();
-const myUrl = "https://api.github.com/users/mukul1107"
-// const reqUrl = `https://api.github.com/users/${name}`;
+const reqUrl = `https://api.github.com/users/${name}`;
 
-function makeRequest() {
 
-    xmlreq.open("GET", myUrl);
-    xmlreq.onreadystatechange = () => {
-        console.log(`Current State is: ${xmlreq.readyState}`);
-        if (xmlreq.readyState == 4) {
-            const content = JSON.parse(xmlreq.responseText);
-            const nameArr = []
-            const followersName = getName(nameArr)
-            console.log(followersName)
-            userName.innerHTML = "Username: " + content.login
-            para.innerHTML = "Followers: " + content.followers;
-            img.src = content.avatar_url;
-            console.log(content.followers)
-        }
-    };
-}
-makeRequest()
+xmlreq.open("GET", reqUrl);
+xmlreq.onreadystatechange = () => {
+    console.log(`Current State is: ${xmlreq.readyState}`);
+    if (xmlreq.readyState == 4) {
+        const content = JSON.parse(xmlreq.responseText);
+        document.querySelector(".name-hd").innerHTML = `===== ${content.login}'s Profile =====`
+        document.querySelector(".user-name").innerHTML = "Username: " + "<em>*" + String(content.login) + "*</em>" 
+        document.querySelector(".followers").innerHTML = String(content.login).slice(0,5) + "'s Followers: " +"<em>"+ content.followers + "</em>";
+        document.querySelector("img").src = content.avatar_url;
+        console.log(content.followers)
+    }
+};
+
+
 
 xmlreq.send();
